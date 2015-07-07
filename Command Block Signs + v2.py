cdb7041591deec08cf2af42085748b596117f2f1
TrazLander <jrayisom@gmail.com>
2015-07-07 03:35:29 -0400

    NAOR

--- Command Block Signs + v2.py
+++ Command Block Signs + v2.py
@@ -115,7 +115,7 @@ inputs = [( ("General","title"),
             "\n"
             "This is a modified version of SethBling's Command Block Signs filter "
             "edited at TrazLander's request. It has gone through several iterations, "
-            "with work done by texelelf, Asdjke, texelelf again, and Naor2013. \n"
+            "with work done by texelelf, Asdjke, texelelf again, and naor2013. \n"
             "Thanks for all their help making this, and all other's feedback in creating this.\n"
             "\n"
             "","label"),
